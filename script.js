c3410958200e6bf07e834298458b6520885ea00e
fatmaali-hub <fatmaalielsayed2020@gmail.com>
2021-11-11 22:04:36 +0200

    firstcommit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("vcolor").oninput = function () {
+    "use strict";
+    var vcolor = document.getElementById("vcolor").value;
+    var res = document.getElementById("res_id");
+     res.style.background=vcolor;   
+} 
+
+
+
+
+
+
+
+
+
